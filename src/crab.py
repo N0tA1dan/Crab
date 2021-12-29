72af81451c3c594ae3dccae2776948083ac90ace
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2021-12-29 00:09:31 -0800

    Update crab.py

--- src/crab.py
+++ src/crab.py
@@ -1,7 +1,9 @@
 import requests
 import sys
-import socket
 import whois
+import threading
+import socket
+
 
 
 args = sys.argv[1:]
@@ -13,6 +15,7 @@ print('''\033[1;31;1m
 / /___/ /  / /_/ / /_/ /
 \____/_/   \__,_/_.___/    By NotAidan.
 ''')
+
 
 def iplookup(host):
     print("\033[1;33;1m-----------------------------------")
@@ -27,24 +30,52 @@ def iplookup(host):
     print("Timezone:", ipsearch['timezone'])
     print("ISP:", ipsearch['isp'])
     print("Organization:", ipsearch['org'])
-  
+
+
 def pytonwhois(host):
-  w = whois.whois(host)
-  print(w.text)
+    w = whois.whois(host)
+    print(w.text)
+
+
+def portscan(host, port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)#
+
+    try:
+        con = s.connect((host,port))
+
+        print('Port :',port,"is open.")
+
+        con.close()
+    except:
+        pass
+
+def portscan2(host):
+    r = 1
+    for x in range(1,10000):
+
+        t = threading.Thread(target=portscan,kwargs={'host':host, 'port': r})
+
+        r += 1
+        t.start()
+
 
 
 try:
-    if(args[0] == "-h"):
+    if (args[0] == "-h"):
         print('''Usage: python3 crab.py [Options] {Target}
     -h: Shows this menu
     -ps: Port Scan - Scans open ports for a given Host
     -i: Info - Get Basic information on a given Host
     -w: whois - Runs a whois search on a given Host
         ''')
-    if(args[0] == "-i"):
+    if (args[0] == "-i"):
         iplookup(args[1])
-    if(args[0] == "-w"):
-      pytonwhois(args[1])
+    if (args[0] == "-w"):
+        pytonwhois(args[1])
+    if(args[0] == "-p"):
+        portscan2(args[1])
 
 except IndexError:
     print("Invalid args. Use [-h] for help")
