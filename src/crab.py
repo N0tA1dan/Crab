9045146eb418bbb4e817e8d3015d09e543658729
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2023-10-21 02:12:43 -0700

    Update crab.py

--- src/crab.py
+++ src/crab.py
@@ -5,6 +5,7 @@ import threading
 import socket
 import time
 from scapy.all import *
+import json
 
 args = sys.argv[1:]
 
@@ -15,6 +16,41 @@ print('''\033[1;31;1m
 / /___/ /  / /_/ / /_/ /
 \____/_/   \__,_/_.___/    By NotAidan.
 ''')
+
+def generate_new_filename(filename):
+    base, ext = os.path.splitext(filename)
+    counter = 1
+    while os.path.exists(f"{base} ({counter}){ext}"):
+        counter += 1
+    return f"{base} ({counter}){ext}"
+
+
+def censysHostLookup(host):
+    headers = {
+        'Accept': 'application/json',
+    }
+
+    params = {
+        'q': f'{host}',
+    }
+
+    response = requests.get(
+        'https://search.censys.io/api/v2/hosts/search',
+        params=params,
+        headers=headers,
+
+        # PUT YOUR API ID AND API KEY HERE IF YOU WANT TO USE CENSYS
+        auth=(os.getenv('CENSYS_API_ID', 'API_ID_HERE'), os.getenv('CENSYS_API_SECRET', 'API_SECRET_HERE')),
+    )
+    
+    filename = "CensysData.txt"
+    new_filename = generate_new_filename(filename)
+    f = open(new_filename, "a+")
+    f.write(json.dumps(response.json(), indent=2))
+    f.close()
+
+    print(f"Check CensysData.txt for the logs or go to https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q={host}")
+
 
 
 # os detection using ttl values in a icmp request
@@ -97,18 +133,26 @@ try:
     if (args[0] == "-h"):
         print('''Usage: python crab.py [Options] {Target}
     -h: Shows this menu
+              
     -sA: Port Scan All - Casual port scan. Scans every port on a given host.
         Usage: python crab.py -sA [host] [time out in seconds]
         Example: python crab.py -sA google.com 0.5
+              
     -sC: Port scan common - Fastest port scan. Only scans from a range of 1 - 1024
         Usage: python crab.py -sC [host] [time out in seconds]
         Example: python crab.py -sC google.com 0.5
+              
     -pD: Ping Detection - this command uses icmp requests and analyzes the ttl to derive an OS.
         --- WARNING --- ping detection might not work unless ran with elevated permissions
         Usage: python crab.py -pD [host]
         Example: python crab.py -pD 1.1.1.1
+              
     -i: Info - Get Basic information on a given Host/IP. Its basically a IP scanner.
+              
     -w: whois - Runs a whois search on a given Host
+              
+    -cS: Censys Lookup - This will run a given domain/host against Censys and obtain good information. REMEMBER TO PLACE YOUR API ID AND API SECRET INTO THE MAIN FILE (crab.py) AROUND LINE 36
+        Usage: python crab.py -cS [host]
         ''')
     if (args[0] == "-i"):
         iplookup(args[1])
@@ -120,6 +164,9 @@ try:
         fastportscan(args[1], args[2])
     if (args[0] == "-pD"):
         icmpOSD(args[1])
+    if (args[0] == "-cS"):
+        censysHostLookup(args[1])
 
 except IndexError:
     print("Invalid args. Use [-h] for help")
+
